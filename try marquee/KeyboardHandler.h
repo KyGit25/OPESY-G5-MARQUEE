accbc0a9fefa28a844c4d526ce926367a0d7d5aa
owensanluis <owenphillipsanluis@gmail.com>
2025-10-01 23:13:45 +0800

    deleted unnecessary files

--- try marquee/KeyboardHandler.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-class KeyboardHandler {
-public:
-    void run();
-    static std::string getInput(const std::string &prompt);
-};
--- try marquee/MarqueeLogic.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class MarqueeLogic {
-public:
-    void run();
-private:
-    void sleepMs(int ms);
-};
